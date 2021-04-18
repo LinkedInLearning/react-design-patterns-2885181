3fb18865338dd2bb8ac3d3754d8cdbb8187af998
Sandy Instructor <sandyludosky@gmail.com>
2021-04-19 01:49:35 +0200

    add 3.3-context-theme

---
+++ fichiers-cours/3-context-api/3.3-context-theme/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ThemeProvider from './ThemeContext'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
