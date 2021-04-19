1db93834ac69d580d0cc024e1733b35c31229c77
Sandy Instructor <sandyludosky@gmail.com>
2021-04-19 15:55:22 +0200

    end

--- fichiers-cours/2-hoc/2.1-hoc-syntax/src/App.js
+++ fichiers-cours/2-hoc/2.1-hoc-syntax/src/App.js
@@ -3,27 +3,29 @@ import reactLogo from "./svg/react-logo.svg";
 import angularLogo from "./svg/angular-logo.svg";
 import vueLogo from "./svg/vuejs-logo.svg";
 
-let libraries = {
-  React: {
-    name: "React",
-    url: "https://reactjs.org/",
-    logo: reactLogo,
-  },
-  Vue: {
-    name: "Vue",
-    url: "https://vuejs.org/",
-    logo: vueLogo,
-    style: { background: "#ecf0f1", color: "#333" },
-  },
-  Angular: {
-    name: "Angular",
-    url: "https://angular.io/",
-    logo: angularLogo,
-    style: { background: "#1976d2" },
-  },
-};
-
 // HOC here
+const withLibrary = (Component) => (props) => {
+  let libraries = {
+    React: {
+      name: "React",
+      url: "https://reactjs.org/",
+      logo: reactLogo,
+    },
+    Vue: {
+      name: "Vue",
+      url: "https://vuejs.org/",
+      logo: vueLogo,
+      style: { background: "#ecf0f1", color: "#333" },
+    },
+    Angular: {
+      name: "Angular",
+      url: "https://angular.io/",
+      logo: angularLogo,
+      style: { background: "#1976d2" },
+    },
+  };
+  return <Component {...libraries[props.library]} />;
+};
 
 function App() {
   return (
