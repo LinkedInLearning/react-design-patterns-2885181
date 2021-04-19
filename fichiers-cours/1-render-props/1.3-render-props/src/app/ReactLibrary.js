3eff67475ee66f782b489b159186b687e39e6fd0
Sandy Instructor <sandyludosky@gmail.com>
2021-04-19 13:22:03 +0200

    end

--- fichiers-cours/1-render-props/1.3-render-props/src/app/ReactLibrary.js
+++ fichiers-cours/1-render-props/1.3-render-props/src/app/ReactLibrary.js
@@ -1,42 +1,39 @@
-import { useState, useEffect } from "react";
 import MouseTrackerWithLogo, { libraries } from "./MouseTrackerWithLogo";
 import reactLogo from "../svg/react-logo.svg";
 import "./App.css";
 
 const ReactLibrary = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const handleOnMouseMove = (event) => {
-    setPosition({
-      x: event.clientX,
-      y: event.clientY,
-    });
-  };
-  useEffect(() => {
-    console.log(position);
-  }, [position]);
-
   return (
-    <div className="App">
-      <header className="App-header" onMouseMove={handleOnMouseMove}>
-        <img
-          src={reactLogo}
-          className="App-logo"
-          alt="logo"
-          style={{ position: "absolute", left: position.x, top: position.y }}
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MouseTrackerWithLogo
+      library={libraries.React}
+      data={({ position, library }) => {
+        return (
+          <>
+            <img
+              src={reactLogo}
+              className="App-logo"
+              alt="logo"
+              style={{
+                position: "absolute",
+                left: position.x,
+                top: position.y,
+              }}
+            />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn {library}
+            </a>
+          </>
+        );
+      }}
+    ></MouseTrackerWithLogo>
   );
 };
 export default ReactLibrary;
