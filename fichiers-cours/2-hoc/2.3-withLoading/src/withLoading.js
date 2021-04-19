416dc27548750c557e91909bb62ff03849f23b4a
Sandy Instructor <sandyludosky@gmail.com>
2021-04-19 18:16:16 +0200

    end

--- fichiers-cours/2-hoc/2.3-withLoading/src/withLoading.js
+++ fichiers-cours/2-hoc/2.3-withLoading/src/withLoading.js
@@ -2,5 +2,11 @@ import { useState, useEffect } from "react";
 
 const Loading = () => <p>En cours de chargement ...</p>;
 
-const withLoading = (Component) => (props) => {};
+const withLoading = (Component) => (props) => {
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000);
+  }, []);
+  return isLoading ? <Loading /> : <Component {...props} />;
+};
 export default withLoading;
