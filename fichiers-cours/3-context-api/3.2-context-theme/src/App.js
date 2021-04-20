682b8dc602691ae08f2591956b71bf44df5284ef
Sandy Instructor <sandyludosky@gmail.com>
2021-04-20 12:46:22 +0200

    end

--- fichiers-cours/3-context-api/3.2-context-theme/src/App.js
+++ fichiers-cours/3-context-api/3.2-context-theme/src/App.js
@@ -1,4 +1,5 @@
 // import ThemeContext
+import ThemeProvider, { ThemeContext } from "./ThemeContext";
 import { useMemo } from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -8,27 +9,33 @@ function App() {
   const buttonStyle = useMemo(() => {}, []);
   return (
     <div className="App">
-      <header className="App-header" style={style}>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <br />
-        <button
-          on
-          style={{ ...buttonStyle, padding: "10px 20px", fontSize: 20 }}
-        >
-          On
-        </button>
-      </header>
+      <ThemeContext.Consumer>
+        {({ on, updateTheme }) => {
+          return (
+            <header className="App-header" style={style}>
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+              <br />
+              <button
+                on
+                style={{ ...buttonStyle, padding: "10px 20px", fontSize: 20 }}
+              >
+                On
+              </button>
+            </header>
+          );
+        }}
+      </ThemeContext.Consumer>
     </div>
   );
 }
